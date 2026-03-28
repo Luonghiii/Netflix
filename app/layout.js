cb435d939bcf19c97dfe6c1b1ab06d60e087f1d1
Luonghiii <luonghii123@gmail.com>
2026-03-29 04:04:19 +0700

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Trung tâm chia sẻ',
+  description: 'Cập nhật link Netflix mới nhất',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="vi">
+      <body>{children}</body>
+    </html>
+  )
+}
